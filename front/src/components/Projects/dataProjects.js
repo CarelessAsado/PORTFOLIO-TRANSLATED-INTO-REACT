216d145a9f51100bcb255da79d1e552c49e5f04b
Rodrigo López <rodrigohernanlopez89@gmail.com>
2022-11-15 17:41:02 -0300

    updated projects

--- front/src/components/Projects/dataProjects.js
+++ front/src/components/Projects/dataProjects.js
@@ -14,22 +14,23 @@ export const dataProjects = [
     github: "",
     web: "https://www.perfumeriamundial.ar/",
   },
+
   {
     descripcion: "",
-    img: "/img/typescript-react.png",
+    img: "/img/rodribook.png",
     alt: "",
     tecnologias: [
-      "React",
-      "Redux-Thunk",
       "Typescript",
-      "JWT",
-      "Node",
-      "Styled-components",
+      "Socket.io",
+      "Firebase",
+      "Redux Thunk",
+      "Cloudinary",
     ],
-    titulo: "",
-    github: "https://github.com/CarelessAsado/Typescript-React-firstApp",
-    web: "https://amazing-wright-70ac6b.netlify.app/",
+    titulo: "RODRIBOOK WEBSITE",
+    github: "https://github.com/CarelessAsado/firebase_nested_comments",
+    web: "https://bright-fudge-e510f2.netlify.app/",
   },
+
   {
     descripcion: "",
     img: "/img/Portfolio.png",
@@ -39,37 +40,9 @@ export const dataProjects = [
     github: "https://github.com/CarelessAsado/PORTFOLIO-TRANSLATED-INTO-REACT",
     web: "https://www.rodrigohernanlopez.com.ar/",
   },
+
   {
     descripcion: "",
-    img: "/img/React+JWT.png",
-    alt: "",
-    tecnologias: ["React", "Nodejs", "JWT", "Mongoose"],
-    titulo: "PROYECTO MERN",
-    github: "https://github.com/CarelessAsado/MERN-TaskManagement",
-    web: "https://frosty-archimedes-bb3dfe.netlify.app/",
-  },
-  {
-    descripcion:
-      "Recreación del famoso sitio. Previo registro e inicio de sesión, los usuarios pueden acceder a un muro público para escribir mensajes, así como acceder de manera privada a su información de usuario y editarla.",
-    img: "/img/rodribook.png",
-    alt: "proyecto Rodribook",
-    tecnologias: ["Nodejs", "Express-sessions", "MongoDb", "Mongoose"],
-    titulo: "RODRIBOOK WEBSITE",
-    github: "https://github.com/CarelessAsado/rodribook",
-    web: "",
-  },
-  {
-    descripcion:
-      "Juego de blackjack que tomé como base del sitio Free Code Camp, y que luego fui adaptando: notificaciones sobre el estado de la partida, un bot dealer, interfaz para realizar apuestas, etc.",
-    img: "/img/blackjack.png",
-    alt: "proyecto Blackjack",
-    tecnologias: ["Javascript", "HTML", "CSS"],
-    titulo: "BLACKJACK GAME",
-    github: "https://github.com/CarelessAsado/jackblack",
-    web: "https://carelessasado.github.io/jackblack/",
-  },
-  {
-    descripcion: "Mi primer sitio web.",
     img: "/img/rohelaw.com.ar.png",
     alt: "proyecto Rohelaw",
     tecnologias: ["Javascript", "HTML", "CSS"],
